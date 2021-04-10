5fec5c2e34b0206cb43d52612b5ba4f9419a5e41
weird-frog <den.6667531@gmail.com>
2021-04-11 00:36:00 +0300

    Add project files.

---
+++ PR7-part2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR7_part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Обеспечьте перенаправление ввода на файл F, а вывода на файл G, и решите следующую задачу: 
+             * В файле F хранятся средние температуры за каждый месяц года. 
+             * В файле G для каждого месяца сохраните отклонение среднемесячной температуры от среднегодовой.
+             * 
+             */
+            StreamReader inFile = new StreamReader("averageTemp.txt");
+            Console.SetIn(inFile);
+
+            // назначение приемника данных, вместо экрана
+            StreamWriter outFile = new StreamWriter("G.txt");
+            Console.SetOut(outFile);
+
+            double[] arr = Array.ConvertAll(Console.ReadLine().Split(','), Double.Parse);
+            double averageYearTemp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                averageYearTemp += arr[i];
+            }
+            averageYearTemp /= 12d;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{Math.Abs(averageYearTemp - arr[i]):f2},");
+            }
+            // закрытие потоков ввода-вывода
+            inFile.Close();
+            outFile.Close();
+        }
+    }
+}
---
+++ PR7/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace PR7
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Занести в файл 10 действительных чисел. Напечатать в конце файла сумму компонентов данного файла
+            try
+            {
+                BinaryWriter fout = new BinaryWriter(new FileStream("binary.dat", FileMode.Create));
+                Random random = new Random();
+                double d = 0, sum = 0;
+
+                int i = 1;
+                for (; i < 11; i++)
+                {
+                    d = Math.Round(random.NextDouble() * 100 - 50, 2); 
+                    fout.Write(d);
+                }
+
+                fout.Close();
+
+                FileStream f = new FileStream("binary.dat", FileMode.Open);
+                BinaryReader fin = new BinaryReader(f);
+
+                try
+                {
+                    while (true)
+                    {
+                        d = fin.ReadDouble();
+                        sum += d;
+                    }
+                }
+                catch (EndOfStreamException e) { }
+
+                fin.Close();
+                f.Close();
+
+                fin = new BinaryReader(new FileStream("binary.dat", FileMode.Open));
+                for (int j = 0; j < 10; j++)
+                {
+                    d = fin.ReadDouble();
+                    Console.Write(d + " ");
+                }
+
+                Console.WriteLine("\nSum = " + sum);
+
+                fin.Close();
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error end: " + e.Message);
+                Console.ReadKey();
+                return;
+            }
+        }
+    }
+}
